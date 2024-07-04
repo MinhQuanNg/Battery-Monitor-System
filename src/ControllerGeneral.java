a382654211be58251602794cf63d14bfd082ef5c
Anh Vo <votqanh@gmail.com>
2024-07-04 12:30:49 +0700

    merge

--- src/ControllerGeneral.java
+++ src/ControllerGeneral.java
@@ -19,6 +19,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -27,16 +28,16 @@ import javafx.stage.Stage;
 public class ControllerGeneral {
     private Stage stage;
     private Scene scene;
-    private Parent root;
     private Gauge gauge;
+
+    @FXML private AnchorPane generalPane;
     @FXML private HBox batteryBox;
-    @FXML private Label maxVLabel, minVLabel, delVLabel, sumVLabel, avgVLabel, maxTLabel, avgTLabel;
     @FXML private GridPane cellPane;
+    @FXML private Label maxVLabel, minVLabel, delVLabel, sumVLabel, avgVLabel, maxTLabel, avgTLabel;
 
-    private double maxV = 0, minV = 0, sumV = 0, maxT = 0, sumT = 0;
+    private double maxV, minV, sumV, maxT, sumT;
 
-
-    public void initialize() {
+    public void initialize() throws IOException {
         gauge = GaugeBuilder.create()
         .skinType(SkinType.BATTERY)
         .animated(true)
@@ -50,12 +51,14 @@ public class ControllerGeneral {
 
         // TODO: get original USB
         DataReader reader = new DataReader(this, Controller.getUSB());
+
+        // Start reading data in separate thread
         Thread thread = new Thread(reader);
         thread.start();
     }
 
     public void back(ActionEvent e) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("ScreenMain.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ScreenMain.fxml"));
         stage = (Stage)((Node) e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
@@ -63,8 +66,8 @@ public class ControllerGeneral {
     }
 
     public void thongtinchitiet(ActionEvent e) {
-        for (Node node : findNodesByClass(root, "general")) {
-            node.setVisible(false);  // Change visibility to hidden
+        for (Node node : findNodesByClass(generalPane, "general")) {
+            node.setVisible(false);  // Hide nodes on screenGeneral
         }
 
         cellPane.setVisible(true);
@@ -75,11 +78,18 @@ public class ControllerGeneral {
         dataScreenDetail(dataArray);
     }
 
-
     // TODO: Refactor
+    // Update screenGeneral
     private void dataScreenGeneral(JSONArray dataArray) throws JSONException {
         final double finaldelV, finalavgV, finalavgT;
         int noCells = dataArray.length();
+        
+        // Reinitialize data
+        maxV = 0;
+        minV = 0;
+        sumV = 0;
+        maxT = 0;
+        sumT = 0;
 
         for (int i = 0; i < noCells; i++) {
             JSONObject dataObject = dataArray.getJSONObject(i);
@@ -93,7 +103,7 @@ public class ControllerGeneral {
                 // Display dung luong
                 Platform.runLater(() -> gauge.setValue(SOC));
 
-                System.out.println("! Battery Level: " + SOC + "%");
+                // System.out.println("Battery Level: " + SOC + "%");
 
                 maxV = voltage;
                 minV = voltage;
@@ -137,49 +147,29 @@ public class ControllerGeneral {
         });
     }
 
-    // Update cell image and data
+    // Update screenDetail
     private void dataScreenDetail(JSONArray dataArray) throws JSONException {
-        String url;
         int noCells = dataArray.length();
+
+        List<Label> cellLabels = findLabels((Parent) cellPane);
+        List<Node> imageViewNodes = findNodesByClass(cellPane, "detailImage");
+        List<Node> dataBoxes = findNodesByClass(cellPane, "detailDataBox");
 
         for (int i = 1; i <= noCells; i++) {
             JSONObject dataObject = dataArray.getJSONObject(i-1);
 
             double voltage = dataObject.optDouble("voltage", Double.NaN);
             double temperature = dataObject.optDouble("temperature", Double.NaN);
-            
-            Label cellLabel = (Label) cellPane.lookup("#cell" + i + "Label");
-            cellLabel.setText("Cell " + i);
 
-            // Update cell image
-            for (Node image : findNodesByClass(cellPane, "generalImage")) {
-                if (temperature == maxT) {
-                    url = "file:images/hot.png";
-                } else {
-                    url = "file:images/normal.png";
-                }
+            int cellNo = i;
 
-                if (voltage == maxV) {
-                    url = "file:images/max.png";
-                } else if (voltage == minV) {
-                    url = "file:images/min.png";
-                }
-                
-                ((ImageView) image).setImage(new Image(url));
-            }
+            // Update cell number label
+            Platform.runLater(() -> cellLabels.get(cellNo-1).setText("Cell " + cellNo));
 
-            // Update cell voltage and temperature
-            for (Node box : findNodesByClass(cellLabel, "generalDataBox")) {
-                List<Label> labels = findLabels((Parent) box);
-                
-                // Voltage label 2 decimal places
-                labels.get(0).setText(String.format("%.2f", voltage));
+            updateCellImage((ImageView) imageViewNodes.get(cellNo-1), voltage, temperature);
+            updateDataLabels((Parent) dataBoxes.get(cellNo-1), voltage, temperature);
 
-                // Temperature label 1 decimal place
-                labels.get(1).setText(String.format("%.1f", temperature));
-            }
-
-            System.out.println("! Cell " + i + ": " + "Voltage: " + voltage + ", Temperature: " + temperature);
+            // System.out.println("Cell " + i + ": " + "Voltage: " + voltage + ", Temperature: " + temperature);
         }
     }
 
@@ -196,16 +186,43 @@ public class ControllerGeneral {
         return matchingNodes;
     }
 
+    // not recursive
     private List<Label> findLabels(Parent root) {
         List<Label> labels = new ArrayList<>();
         for (Node node : root.getChildrenUnmodifiable()) {
           if (node instanceof Label) {
             labels.add((Label) node);
           }
-          if (node instanceof Parent) {
-            labels.addAll(findLabels((Parent) node));
-          }
         }
         return labels;
-      }
+    }
+
+    private void updateCellImage(ImageView node, double V, double T) {
+        String url = "images/normal.png";
+
+        if (T == maxT) {
+            url = "images/hot.png";
+        }
+
+        if (V == maxV) {
+            url = "images/max.png";
+        } else if (V == minV) {
+            url = "images/min.png";
+        }
+
+        Image image = new Image(getClass().getResourceAsStream(url));
+        Platform.runLater(() -> node.setImage(image));
+    }
+
+    private void updateDataLabels(Parent box, double V, double T) {
+        List<Label> labels = findLabels(box);
+
+        Platform.runLater(() -> {
+            // Voltage label 2 decimal places
+            labels.get(0).setText(String.format("%.2f", V) + "V");
+
+            // Temperature label 1 decimal place
+            labels.get(1).setText(String.format("%.1f", T) + "°C");
+        });
+    }
 }
