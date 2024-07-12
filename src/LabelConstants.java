cf8f9ebb85c1560965c72b2475c0903fa36f048c
lilcrabie <myspace.daniel35@gmail.com>
2024-07-12 09:07:48 +0700

    update General

--- src/LabelConstants.java
+++ src/LabelConstants.java
@@ -5,7 +5,17 @@ public class LabelConstants {
     public static final String VNM_REAL_TIME = "Quan sát dữ liệu thời gian thực";
     
     // VNM ScreenGeneral labels
-
+    public static final String VNM_TITLE = "HỆ THỐNG QUẢN LÝ PIN";
+    public static final String VNM_MAXVOLTAGE = "Điện áp tối đa";
+    public static final String VNM_MINVOLTAGE = "Điện áp tối thiểu";
+    public static final String VNM_SUM = "Tổng áp";
+    public static final String VNM_MAXTEMPERATURE = "Nhiệt độ tối đa";
+    public static final String VNM_MINTEMPERATURE = "Nhiệt độ tối thiểu";
+    public static final String VNM_AVE= "Trung bình";
+    public static final String VNM_ERROR = "Cảnh báo lỗi";
+    public static final String VNM_GEN = "Thông tin chung";
+    public static final String VNM_DE = "Thông tin chi tiết";
+    public static final String VNM_PRO = "Hồ sơ thiết lập";
 
     // ENG ScreenMain labels
     public static final String ENG_LOOKING_FOR_DEVICES = "SEARCHING FOR DEVICES….";
@@ -13,6 +23,16 @@ public class LabelConstants {
     public static final String ENG_REAL_TIME = "Real-time data";
 
     // ENG ScreenGeneral labels
-
+    public static final String ENG_TITLE = "BATTERY MANAGEMENT SYSTEM";
+    public static final String ENG_MAXVOLTAGE = "Max Voltage";
+    public static final String ENG_MINVOLTAGE = "Min Voltage";
+    public static final String ENG_SUM = "Total Voltage";
+    public static final String ENG_MAXTEMPERATURE = "Max Temperature";
+    public static final String ENG_MINTEMPERATURE = "Min Temperature";
+    public static final String ENG_AVE= "Average";
+    public static final String ENG_ERROR = "Error Warning";
+    public static final String ENG_GEN = "General Information";
+    public static final String ENG_DE = "Detail Information";
+    public static final String ENG_PRO = "Profile Settings";
     
 }
