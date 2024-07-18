ed8509aedd1c001d6a8226c6fb148cebebd12828
Anh Vo <votqanh@gmail.com>
2024-07-18 12:06:55 +0700

    fix title & legend

--- src/test/CellChartTest.java
+++ src/test/CellChartTest.java
@@ -1,7 +1,10 @@
 package test;
 
 import controllers.CellChart;
+import controllers.Charts;
 
+import java.util.Date;
+import java.util.Hashtable;
 
 import constants.Style;
 import javafx.application.Application;
@@ -14,10 +17,22 @@ public class CellChartTest extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            CellChart cellChart = new CellChart(1);
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../resources/CellChart.fxml"));
-            loader.setController(cellChart);
+            // CellChart cellChart = new CellChart(1);
+            // FXMLLoader loader = new FXMLLoader(getClass().getResource("../resources/CellChart.fxml"));
+            // loader.setController(cellChart);
+
+            Hashtable<String, Double> data = new Hashtable<>();
+            data.put("maxV", 3.3);
+            data.put("minV", 2.4);
+            data.put("sumV", 96.0);
+            data.put("maxT", 32.1);
+            data.put("delV", 0.07);
+
+            Charts charts = new Charts(1, new Date());
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../resources/Charts.fxml"));
+            loader.setController(charts);
             Parent root = loader.load();
+            charts.initSeries(data);
             Scene chartScene = new Scene(root, Style.CHART_WIDTH, Style.CHART_HEIGHT);
 
             Stage chartStage = new Stage();
